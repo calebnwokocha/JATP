f2c55c67112c75c833eebc9762a1a059b0c4c4da
Caleb P. Nwokocha <calebnwokocha@gmail.com>
2024-09-12 15:51:41 -0500

    test

--- JATP.java
+++ JATP.java
@@ -1,81 +1,104 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class JATP {
-    private final HashMap<Object, Object> memory = new HashMap<>();
+    private final Scanner scanner = new Scanner(System.in);
+    private final HashMap<String, String> memory = new HashMap<>();
+    private final HashMap<String, String> reverseMemory = new HashMap<>();
 
     public JATP() {
         System.out.println("JAVA AUTOMATED THEOREM PROVER (JATP)");
         System.out.println("Copyright: Caleb Princewill Nwokocha");
         System.out.println();
-        System.out.println("Theorem: Every equation has a left object and a right object.");
-        System.out.println("Example 1: a is left object of a=b and b is its right object.");
-        System.out.println("Example 2: a is left object of a=a and a is its right object.");
-        System.out.println("Example 3: a+b is left object of a+b=b+a and b+a is its right object.");
-        System.out.println("Example 4: a+b is left object of a+b=a+b and a+b is its right object.");
+        System.out.println("Theorem: Every equation has a left string and a right string.");
+        System.out.println("Example 1: a is left string of a=b and b is its right string.");
+        System.out.println("Example 2: a is left string of a=a and a is its right string.");
+        System.out.println("Example 3: a+b is left string of a+b=b+a and b+a is its right string.");
+        System.out.println("Example 4: a+b is left string of a+b=a+b and a+b is its right string.");
         System.out.println();
         this.load();
     }
 
-    public void input(Object leftObject, Object rightObject) {
-        if (this.memory.containsKey(leftObject)) {
-            output(leftObject);
+    public void input(String leftString, String rightString) {
+        if (this.memory.containsKey(leftString) &&
+                this.memory.get(leftString).equals(rightString)) {
+            output(leftString);
         } else {
-            this.memory.put(leftObject, rightObject);
-            prove(leftObject);
+            prove(leftString, rightString);
         }
     }
 
-    private void output(Object leftObject) {
-        System.out.println("equation found: " + leftObject + "=" + this.memory.get(leftObject)
-                + "=" + this.memory.get(this.memory.get(leftObject)));
+    private void output(String leftString) {
+        System.out.println("theorem found: " + leftString + "=" + this.memory.get(leftString)
+                + "=" + this.reverseMemory.get(leftString)
+                + "=" + this.memory.get(this.memory.get(leftString))
+                + "=" + this.reverseMemory.get(this.reverseMemory.get(leftString))
+                + "=" + this.memory.get(this.reverseMemory.get(leftString))
+                + "=" + this.reverseMemory.get(this.memory.get(leftString)));
         System.out.println("memory map: " + this.memory);
-        this.save();
+        System.out.println("reverse memory map: " + this.reverseMemory);
     }
 
-    private void prove(Object leftObject) {
-        ArrayList<Object> leftObjects = new ArrayList<>(this.memory.keySet());
-        for (Object object : leftObjects) {
-            Object rightObject = this.memory.get(object);
-            if (!object.equals(leftObject) && rightObject.equals(this.memory.get(leftObject))) {
-                this.memory.replace(leftObject, object);
+    private void prove(String leftString, String rightString) {
+        if (this.memory.containsKey(leftString)) {
+            System.out.println("contradiction found: " + leftString + "≠" + rightString);
+            System.out.println("equation found: " + leftString + "=" + this.memory.get(leftString));
+            System.out.print("redefinition: " + leftString + "=");
+            rightString = this.scanner.nextLine();
+            if (!this.reverseMemory.get(rightString).equals(leftString)){
+                this.memory.replace(leftString, rightString);
+                this.reverseMemory.replace(rightString, leftString);
+                this.save(leftString, rightString);
             }
+        } else {
+            this.memory.put(leftString, rightString);
+            this.reverseMemory.put(rightString, leftString);
+            this.save(leftString, rightString);
         }
     }
 
-    private void save() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("memory.jatp"))) {
-            oos.writeObject(this.memory);
-            System.out.println("memory map saved");
+    private void save(String leftString, String rightString) {
+        try (DataOutputStream dos = new DataOutputStream(new
+                FileOutputStream("memory.jatp", true))) {
+            dos.writeUTF(leftString.toString());
+            dos.writeUTF(rightString.toString());
+            System.out.println("saved " + leftString + "=" + rightString);
         } catch (IOException e) {
-            System.out.println("error saving memory map: " + e.getMessage());
+            System.out.println("error saving " + leftString + "=" + rightString + "; " + e.getMessage());
         }
     }
 
-    @SuppressWarnings("unchecked")
     private void load() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("memory.jatp"))) {
-            HashMap<Object, Object> loadedMemory = (HashMap<Object, Object>) ois.readObject();
-            this.memory.clear();
-            this.memory.putAll(loadedMemory);
+        this.memory.clear();
+        this.reverseMemory.clear();
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream("memory.jatp"))) {
+            while (true) {
+                try {
+                    String leftString = dis.readUTF();
+                    String rightString = dis.readUTF();
+                    this.memory.put(leftString, rightString);
+                    this.reverseMemory.put(rightString, leftString);
+                } catch (EOFException e) {
+                    break;
+                }
+            }
             System.out.println("memory map loaded");
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("error loading memory map: " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
         JATP JATP = new JATP();
-        Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("left object: ");
-            String leftObject = scanner.nextLine();
-            System.out.print("right object: ");
-            String rightObject = scanner.nextLine();
-            JATP.input(leftObject, rightObject);
+            System.out.print("left string: ");
+            String leftString = JATP.scanner.nextLine();
+            System.out.print("right string: ");
+            String rightString = JATP.scanner.nextLine();
+            JATP.input(leftString, rightString);
         }
     }
 }
