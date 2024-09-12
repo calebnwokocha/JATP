0ceb8b46cfb7b5ce66d24512a5597a4b67c31677
Caleb P. Nwokocha <calebnwokocha@gmail.com>
2024-09-12 16:56:10 -0500

    test

--- JATP.java
+++ JATP.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class JATP {
     private final Scanner scanner = new Scanner(System.in);
-    private final HashMap<String, String> memory = new HashMap<>();
-    private final HashMap<String, String> reverseMemory = new HashMap<>();
+    private final HashMap<String, String> map = new HashMap<>();
+    private final HashMap<String, String> reverseMap = new HashMap<>();
 
     public JATP() {
         System.out.println("JAVA AUTOMATED THEOREM PROVER (JATP)");
@@ -21,8 +21,8 @@ public class JATP {
     }
 
     public void input(String leftString, String rightString) {
-        if (this.memory.containsKey(leftString) &&
-                this.memory.get(leftString).equals(rightString)) {
+        if (this.map.containsKey(leftString) &&
+                this.map.get(leftString).equals(rightString)) {
             output(leftString);
         } else {
             prove(leftString, rightString);
@@ -30,30 +30,31 @@ public class JATP {
     }
 
     private void output(String leftString) {
-        System.out.println("theorem found: " + leftString + "=" + this.memory.get(leftString)
-                + "=" + this.reverseMemory.get(leftString)
-                + "=" + this.memory.get(this.memory.get(leftString))
-                + "=" + this.reverseMemory.get(this.reverseMemory.get(leftString))
-                + "=" + this.memory.get(this.reverseMemory.get(leftString))
-                + "=" + this.reverseMemory.get(this.memory.get(leftString)));
-        System.out.println("memory map: " + this.memory);
-        System.out.println("reverse memory map: " + this.reverseMemory);
+        System.out.println("theorem: " + leftString + "=" + this.map.get(leftString)
+                + "=" + this.reverseMap.get(leftString)
+                + "=" + this.map.get(this.map.get(leftString))
+                + "=" + this.reverseMap.get(this.reverseMap.get(leftString))
+                + "=" + this.map.get(this.reverseMap.get(leftString))
+                + "=" + this.reverseMap.get(this.map.get(leftString)));
+        System.out.println("map: " + this.map);
+        System.out.println("reverse map: " + this.reverseMap);
     }
 
     private void prove(String leftString, String rightString) {
-        if (this.memory.containsKey(leftString)) {
-            System.out.println("contradiction found: " + leftString + "≠" + rightString);
-            System.out.println("equation found: " + leftString + "=" + this.memory.get(leftString));
+        if (this.map.containsKey(leftString)) {
+            System.out.println("contradiction: " + leftString + "≠" + rightString);
+            System.out.println("recall: " + leftString + "=" + this.map.get(leftString));
             System.out.print("redefinition: " + leftString + "=");
-            rightString = this.scanner.nextLine();
-            if (!this.reverseMemory.get(rightString).equals(leftString)){
-                this.memory.replace(leftString, rightString);
-                this.reverseMemory.replace(rightString, leftString);
-                this.save(leftString, rightString);
+            String newRightString = this.scanner.nextLine();
+            if (newRightString.equals(rightString)){
+                this.reverseMap.remove(this.map.get(leftString));
+                this.reverseMap.put(newRightString, leftString);
+                this.map.replace(leftString, newRightString);
+                this.save(leftString, newRightString);
             }
         } else {
-            this.memory.put(leftString, rightString);
-            this.reverseMemory.put(rightString, leftString);
+            this.map.put(leftString, rightString);
+            this.reverseMap.put(rightString, leftString);
             this.save(leftString, rightString);
         }
     }
@@ -61,8 +62,8 @@ public class JATP {
     private void save(String leftString, String rightString) {
         try (DataOutputStream dos = new DataOutputStream(new
                 FileOutputStream("memory.jatp", true))) {
-            dos.writeUTF(leftString.toString());
-            dos.writeUTF(rightString.toString());
+            dos.writeUTF(leftString);
+            dos.writeUTF(rightString);
             System.out.println("saved " + leftString + "=" + rightString);
         } catch (IOException e) {
             System.out.println("error saving " + leftString + "=" + rightString + "; " + e.getMessage());
@@ -70,16 +71,16 @@ public class JATP {
     }
 
     private void load() {
-        this.memory.clear();
-        this.reverseMemory.clear();
+        this.map.clear();
+        this.reverseMap.clear();
 
         try (DataInputStream dis = new DataInputStream(new FileInputStream("memory.jatp"))) {
             while (true) {
                 try {
                     String leftString = dis.readUTF();
                     String rightString = dis.readUTF();
-                    this.memory.put(leftString, rightString);
-                    this.reverseMemory.put(rightString, leftString);
+                    this.map.put(leftString, rightString);
+                    this.reverseMap.put(rightString, leftString);
                 } catch (EOFException e) {
                     break;
                 }
