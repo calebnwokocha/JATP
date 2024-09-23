671708d639fdb0de25e6cfd58414977ccddba786
Caleb P. Nwokocha <calebnwokocha@gmail.com>
2024-09-23 16:53:25 -0500

    test

--- JATP.java
+++ JATP.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class JATP {
-    private final HashMap<String, String> equationMap = new HashMap<>();
+    private final HashMap<String, String> map = new HashMap<>();
     private final Scanner scanner = new Scanner(System.in);
 
     public JATP() {
@@ -28,42 +28,42 @@ public class JATP {
     }
 
     private void output(String leftString) {
-        if (this.equationMap.get(this.equationMap.get(leftString)) != null){
-            System.out.println("Theorem: " + leftString + "=" + this.equationMap.get(leftString)
-                    + "=" + this.equationMap.get(this.equationMap.get(leftString)));
+        if (this.map.get(this.map.get(leftString)) != null){
+            System.out.println("Theorem: " + leftString + "=" + this.map.get(leftString)
+                    + "=" + this.map.get(this.map.get(leftString)));
         } else {
-            this.define(this.equationMap.get(leftString));
+            this.define(this.map.get(leftString));
         }
     }
 
     private void define(String leftString) {
         System.out.print("Definition: " + leftString + "=");
         String rightString = this.scanner.nextLine();
-        this.equationMap.put(leftString, rightString);
+        this.map.put(leftString, rightString);
         this.save(leftString, rightString);
     }
 
     private void redefine(String leftString, String rightString) {
         System.out.print("Redefinition: " + leftString + "=");
         rightString = this.scanner.nextLine();
-        if (!rightString.equals(this.equationMap.get(leftString))) {
-            this.equationMap.replace(leftString, rightString);
+        if (!rightString.equals(this.map.get(leftString))) {
+            this.map.replace(leftString, rightString);
             this.save(leftString, rightString);
         }
     }
 
     private boolean prove (String leftString, String rightString) {
-        return this.equationMap.containsKey(leftString) && this.equationMap.get(leftString).equals(rightString);
+        return this.map.containsKey(leftString) && this.map.get(leftString).equals(rightString);
     }
 
     private void disprove(String leftString, String rightString) {
-        if (this.equationMap.containsKey(leftString)) {
+        if (this.map.containsKey(leftString)) {
             System.out.println("Conjecture: " + leftString + "=" + rightString);
-            System.out.println("Recall: " + leftString + "=" + this.equationMap.get(leftString));
-            System.out.println("Equation Map: " + this.equationMap);
+            System.out.println("Recall: " + leftString + "=" + this.map.get(leftString));
+            System.out.println("Map: " + this.map);
             this.redefine(leftString, rightString);
         } else {
-            this.equationMap.put(leftString, rightString);
+            this.map.put(leftString, rightString);
             this.save(leftString, rightString);
         }
     }
@@ -80,13 +80,13 @@ public class JATP {
     }
 
     private void load() {
-        this.equationMap.clear();
+        this.map.clear();
         try (DataInputStream DIS = new DataInputStream(new FileInputStream("map.jatp"))) {
             while (true) {
                 try {
                     String leftString = DIS.readUTF();
                     String rightString = DIS.readUTF();
-                    this.equationMap.put(leftString, rightString);
+                    this.map.put(leftString, rightString);
                 } catch (EOFException e) {
                     break;
                 }
@@ -100,9 +100,9 @@ public class JATP {
     public static void main(String[] args) {
         JATP JATP = new JATP();
         while (true) {
-            System.out.print("Left String: ");
+            System.out.print("Left string: ");
             String leftString = JATP.scanner.nextLine();
-            System.out.print("Right String: ");
+            System.out.print("Right string: ");
             String rightString = JATP.scanner.nextLine();
             JATP.input(leftString, rightString);
         }
